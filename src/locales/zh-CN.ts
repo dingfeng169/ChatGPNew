e581a26cd5d15de29b57af990bffa9932d734456
dingfeng169 <64767075+dingfeng169@users.noreply.github.com>
2023-03-16 21:04:16 +0800

    Update zh-CN.ts

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -25,7 +25,9 @@ export default {
     success: '操作成功',
     failed: '操作失败',
     verify: '验证',
-    unauthorizedTips: '未经授权，请先进行验证。',
+    unauthorizedTips: '未经授权，请先进行验证。<p style="text-align:center;">
+	<a href="https://www.zuuu.net/details/4FBA4B2A" target="_blank"><span style="color:#006600;font-size:16px;"><strong>点击获取永久授权码</strong></span></a>
+</p>',
   },
   chat: {
     placeholder: '来说点什么吧...（Shift + Enter = 换行）',
